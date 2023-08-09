ebf07f8b3b37594415542c8756c9b00a6b904a13
Gabriel Dias <gabrielhpdias33@gmail.com>
2023-08-09 08:37:46 -0300

    Update and Remove

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 
-export const Task = ({task}) => {
-    return <li>{task.text}</li>
+export const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
+    return (
+        <li>
+            <input 
+                type="checkbox" 
+                checked={!!task.isChecked} //o duplo ! transforma o dado em um booleano correspondente
+                onClick={() => onCheckboxClick(task)}
+                readOnly
+            />
+            <span>{task.text}</span>
+            <button onClick={() => onDeleteClick(task)}>&times;</button>
+        </li>
+    );
 };
